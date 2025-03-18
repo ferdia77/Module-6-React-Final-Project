b9e37f5d48caf5fd0a1064721dd3e5376d4cb807
David <kingqueensuited22@gmai.com>
2025-03-18 16:40:26 +0100

    a

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -2,24 +2,24 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-const Details = () => {
+const Details = ({ movie }) => {
     const { imdbID } = useParams()
-    const[movie, setMovie] = useState([])
+    const[movieId, setMovieId] = useState([])
 
 
     useEffect(() => {
         async function fetchImdbID() {
             try{
-                const movieImdbId = await axios.get(
+                const response = await axios.get(
                     `https://omdbapi.com/?i=${imdbID}&page=1&apikey=e3aee4a2`
                     );
-                    setMovie(movieImdbId)
+                    setMovieId(response.data)
             } catch (error) {
                 console.error("Error fetching movie data:", error)
             }
             
         }
-        fetchImdbID()
+        fetchImdbID();
     }, [imdbID])
 
     
@@ -31,19 +31,19 @@ const Details = () => {
           <figure className="movie__poster">
             <img
                 className="movie__poster--size"
-              src="https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg"
-              alt=""
+              src={movie.Poster}
+              alt={movie.Title}
             />
           </figure>
         </div>
 
         <div className="movies__wrapper">
-          <div className="movie__details"><span className="title__size">Title:</span> {movie.Title}</div>
-          <div className="movie__details"><span className="title__size">Year:</span> {movie.Year}</div>
-          <div className="movie__details"><span className="title__size">Genre:</span> {movie.Genre}</div>
-          <div className="movie__details"><span className="title__size">Director:</span> {movie.Director}</div>
-          <div className="movie__details"><span className="title__size">Actors:</span> {movie.Actors}</div>
-          <div className="movie__details plot__details"><span className="title__size">Plot:</span> <br /> <br />{movie.Plot}</div>
+          <div className="movie__details"><span className="title__size">Title:</span> {movieId.Title}</div>
+          <div className="movie__details"><span className="title__size">Year:</span> {movieId.Year}</div>
+          <div className="movie__details"><span className="title__size">Genre:</span> {movieId.Genre}</div>
+          <div className="movie__details"><span className="title__size">Director:</span> {movieId.Director}</div>
+          <div className="movie__details"><span className="title__size">Actors:</span> {movieId.Actors}</div>
+          <div className="movie__details plot__details"><span className="title__size">Plot:</span> <br /> <br />{movieId.Plot}</div>
         </div>
       </div>
     </div>
